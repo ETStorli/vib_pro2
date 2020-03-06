e17bf3dc4a0a04a7051cba1b6e8cb41030cc8ad8
Sindre <Sindrevjor@gmail.com>
2020-03-06 14:53:20 +0100

    rettet gradient feil og u_j heter nå optimering.

--- py_script/main.py
+++ py_script/main.py
@@ -56,7 +56,7 @@ def YK(Y0, K = K, sigma = sigma, h = h, Wk = Wk, bk = bk):
 # Wk er tredimensjonell, bk er todimensjonell, Y er array med alle Y-matrisene
 # Disse brukes for å regne ut gradienten for utregning av parametrene som brukes i neste lag
 def gradient(Wk, bk, w, mu, Y, C):
-    J_mu = d_eta(np.transpose(np.transpose(Y[-1])@w + mu*one)) * (Z(Y[-1]) - C)
+    J_mu = d_eta(np.transpose(np.transpose(Y[-1])@w + mu*one)) @ (Z(Y[-1]) - C)
     J_w = Y[-1]@((Z(Y[-1]) - C)*d_eta(np.transpose(Y[-1])@w + mu))
     PK = np.array([np.outer(w, np.transpose((Z(Y[-1]) - C) * d_eta(np.transpose(Y[-1]) @ w + mu*one)))])
 
@@ -104,6 +104,7 @@ def y0():
     Z = np.vstack((X, Y))
     return np.array((np.array((xFalse, yFalse)), np.array((xTrue, yTure)))), C, Z
 
+############################################
 
 #Her er gradient _J_U listen med J derivert på div element. U_j inneholder [W_K, B_k, w, my]
 def adam_decent(gradient_J_U, U_j, j):              #her må man ta inn j som teller iterasjonstallet
@@ -131,17 +132,25 @@ def adam_decent(gradient_J_U, U_j, j):              #her må man ta inn j som te
             U_j[u] = U_j[u] - alpha*(m_j_hatt/(np.sqrt(v_hatt)+litn_epsilon))       #U_jp1 = U_(j+1)
     return U_j
 
+##################################################
+
 j = 0
 tau = 0.01            #Læringsparameter. Vi skal bruke det som konvergerer raskest på intervallet [0.01,0.1]
 
-def u_j(U):           #Returnerer de oppdaterte parameterene for neste iterasjon
-    tau = [.1, .01]
 
-    U[0] = U[0] - tau[0]*U[0]
-    U[1] = U[1] - tau[0]*U[1]
-    U[2] = U[2] - tau[0]*U[2]
-    U[3] = U[3] - tau[0]*U[3]
-    return U
+def optimering(grad_U, U_j):           #Returnerer de oppdaterte parameterene for neste iterasjon
+    tau = [.1, .01]
+    """
+    #print("U_J array :", U_j)
+    print("Grad U[3]", grad_U[3])
+    
+    print("grad_U array: ", grad_U)
+    """
+    U_j[0] = U_j[0] - tau[0]*grad_U[0]
+    U_j[1] = U_j[1] - tau[0]*grad_U[1]
+    U_j[2] = U_j[2] - tau[0]*grad_U[2]
+    U_j[3] = U_j[3] - tau[0]*grad_U[3]
+    return U_j
 
 ##############################################################################
 #Selve programmet som kjenner igjen bildene
@@ -154,22 +163,54 @@ def algoritme(N,grad,K=K,sigma=sigma,h=h,Wk=Wk,bk=bk, w=w, mu = mu):
         _, C, Y0 = y0()
         Yk = YK(Y0)         # Array med K Yk matriser, kjører bildene igjennom alle lagene ved funk. YK
         d_Wk, d_bk, d_w, d_mu = grad(Wk,bk,w,mu,Yk,C)                # Regner ut gradieinten for parametrene våre
-        Wk, bk, w, mu = u_j([d_Wk, d_bk, d_w, d_mu])                 # Oppdaterer parametrene vhp. u_j
+        Wk, bk, w, mu = optimering([d_Wk, d_bk, d_w, d_mu], [Wk, bk, w, mu])     # Oppdaterer parametrene vhp. u_j
+        #print("wk, ", Wk,"bk: ", bk, "w: ", w, "my, ",  mu)
         j += 1
     return Yk[-1], Wk, bk, w, mu
 
-Y_K, Wk, bk, w, mu = algoritme(5,gradient)
+Y_K, Wk, bk, w, mu = algoritme(5, gradient)
 
 # 1)
 # Per nå kjøres SAMME Y0 igjennom modellen vår. Rett?
 # Comment på commenten: Har gjort det slik at vi henter en ny Y0 for hver iterasjon
 
 m, _, _= y0()
+"""
 plt.plot(m[0][0], m[0][1], '.')
 plt.plot(m[1][0], m[1][1], '.')
 plt.show()
+"""
+
+
+"""
+print(len(Y_K[0]))
+
+"""
+
+def split_YK(Y_k):
+    Y_false = np.array([])
+    Y_true = np.array([])
+    for i in range(0, int(I/2)):
+        Y_false = np.append([Y_K[0][int(i)]][Y_K[1][int(i)]])
+    for j in range(int(I/2), I):
+        Y_true = [[Y_K[0][i]][Y_K[1][i]]]
+    return Y_false, Y_true
+
+
+
+Y_false , Y_true = split_YK(Y_K)
+
+print(split_YK(Y_K))
+
+"""
+plt.figure()
+plt.plot(Y_false[0], Y_false[1], '.', color = "r")
+plt.plot(Y_true[0], Y_true[1], '.', color = "b")
+plt.show()
+"""
 
 # 2)
+
 # Må ha funk som kan plotte Y_K, det siste bildet etter alle lagene. Slik det er satt opp,
 # er de første I/2 bildene False, og de siste I/2 bildene er True
 
